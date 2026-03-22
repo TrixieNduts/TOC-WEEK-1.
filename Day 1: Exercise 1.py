1707e3f4212afb3bf1cc83156fff810e07f4159a
Trixie <annabeat08@gmail.com>
2026-03-22 17:41:21 +0300

    Finally started the TOC challenge

--- Day 1: Exercise 1.py
+++ Day 1: Exercise 1.py
@@ -3,7 +3,7 @@ import sys
 def main():
 
     name = input(" What is your name? ")
-    print(f"Hello {name}, welcome to TOC challenge!")
+    print(f"Hello {name}, welcome to TOC Python challenge!")
     return 0
 
 if __name__ == "__main__":
