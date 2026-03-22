9f4ece2f05830d4a584bb943f1e5660aaf62f6d0
Trixie <annabeat08@gmail.com>
2026-03-22 18:01:57 +0300

    Finally started the TOC challenge

---
+++ Day 1: Exercise 2.py
@@ -0,0 +1,11 @@
+import sys
+
+def main():
+
+    x = float(input(" Kindly input the value of X ="))
+    y = float(input(" Kindly input the value of Y ="))
+
+    print(f"SUM = {x + y}")
+    return 0
+if __name__ == "__main__":
+    sys.exit(main())
